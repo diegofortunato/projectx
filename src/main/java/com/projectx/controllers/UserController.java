09d32a9db1b7e2dc7c73d6ca64c1f9f478394c33
Fernando Souza <fernando.souza@dextra-sw.com>
2020-05-01 01:27:57 -0300

    cross

--- src/main/java/com/projectx/controllers/UserController.java
+++ src/main/java/com/projectx/controllers/UserController.java
@@ -4,11 +4,13 @@ import com.projectx.model.User;
 import com.projectx.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
 	private UserService userService;
